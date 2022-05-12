836f05f6917faa9ca36f70a5e5ed2c5044f936e0
Sandhya16mca <103841846+Sandhya16mca@users.noreply.github.com>
2022-05-13 01:39:36 +0530

    Add files via upload

---
+++ NumberOfRowsAndColumns.java
@@ -0,0 +1,28 @@
+package week4.day1.assignments;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class NumberOfRowsAndColumns {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get(" https://html.com/tags/table/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		int tablecount = driver.findElements(By.xpath("(//table)")).size();
+		System.out.println("Total number of tables in the page is - " + tablecount);
+		System.out.println("-------------------------------------------------");
+		for (int i = 1; i <= tablecount; i++) {
+			int table1rowcount = driver.findElements(By.xpath("(//table)[" + i + "]//tr")).size();
+			System.out.println("Number of rows in table " + i + " is " + table1rowcount);
+			int table1colcount = driver.findElements(By.xpath("(//table)[" + i + "]//th")).size();
+			System.out.println("Number of column in table " + i + " is " + table1colcount);
+			System.out.println("-------------------------------------------------");
+		}
+
+	}
+
+}
